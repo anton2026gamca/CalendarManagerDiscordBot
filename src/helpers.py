1c9f83d5987d63dfcbc81ccd8e73db51a21dd6f8
Anton Abraham <Abraham1@gamca.sk>
2025-01-10 20:01:21 +0100

    Add files via upload

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 
 def load_json(file_path: str):
     with open(file_path, 'r') as file:
@@ -8,3 +9,10 @@ def write_json(file_path: str, objs):
     json_obj: str = json.dumps(objs)
     with open(file_path, "w") as file:
         file.write(json_obj)
+
+def is_date_valid(day: int, month: int, year: int) -> bool:
+    try:
+        datetime.date(year, month, day)
+        return True
+    except ValueError:
+        return False
